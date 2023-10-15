efbc580c364302a2d058b742b5c0b596024aab40
joaofrancisco601 <114101511+joaofrancisco601@users.noreply.github.com>
2023-10-15 20:22:27 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,16 +1,1 @@
-# myFirstStreamlitApp.py
-  
-#import the library
-import streamlit as st
-  
-# Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("A GALERA DO BENTÃO É FERA!")
 
-# Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Cabeçalho")
-
-# Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Sub Cabeçalho")
-
-# Use st.write("") para adicionar um texto ao seu Web app
-st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
